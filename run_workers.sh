73de7b74b9a6a3e6f840a9283dde671d5d202cbb
LSikyr <sikyrynskyi@gmail.com>
2019-07-24 15:44:46 +0300

    working worker

--- run_workers.sh
+++ run_workers.sh
@@ -6,10 +6,9 @@ export WORKER_EMAIL_PASSWORD="qwertyytrewq13242151"
 export MAIL_SERVER="smtp.gmail.com"
 export MAIL_SERVER_PORT=465
 export PATH_TO_EXPORT_FILES="./export_workers/files_to_export"
-export RABBITMQ_HOST=172.17.0.2
-export RABBITMQ_PORT=5672
 PATH_TO_CREDENTIALS="./export_workers/workers/google_drive"
 export PATH_TO_CREDENTIALS
+sleep 10
 python3 ./export_workers/workers/create_file/create_files.py &
 python3 ./export_workers/workers/send_email/email_listener.py &
 python3 ./export_workers/workers/google_drive/google_drive_listener.py &
