1ccc97e519976a654499eccc35fe771de7595631
LSikyr <sikyrynskyi@gmail.com>
2019-07-25 12:47:30 +0300

    fixed bash script that run workers

--- run_workers.sh
+++ run_workers.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -e
+
+until timeout 1 bash -c "cat < /dev/null > /dev/tcp/${RABBITMQ_HOST}/${RABBITMQ_PORT}"; do
+  >&2 echo "Rabbit MQ not up yet on ${RABBITMQ_HOST}"
+  sleep 10
+done
+
+echo "Rabbit MQ is up"
+
+# do something
 PYTHONPATH="${PYTHONPATH}:/export_workers"
 export PYTHONPATH
 export WORKER_EMAIL_ADDRESS="4m.export.service@gmail.com"
@@ -8,7 +18,7 @@ export MAIL_SERVER_PORT=465
 export PATH_TO_EXPORT_FILES="./export_workers/files_to_export"
 PATH_TO_CREDENTIALS="./export_workers/workers/google_drive"
 export PATH_TO_CREDENTIALS
-sleep 10
+
 python3 ./export_workers/workers/create_file/create_files.py &
 python3 ./export_workers/workers/send_email/email_listener.py &
 python3 ./export_workers/workers/google_drive/google_drive_listener.py &
